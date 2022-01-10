b8e031e9ef310ba511298d4458a9d409dad65551
Shockch4rge <favouriteteo@gmail.com>
2022-01-11 03:16:45 +0800

    Properly check store for user auth state

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -1,5 +1,13 @@
-import store from "../store/index"
+import { useEffect, useState } from "react";
+import store from "../store/index";
 
 export const useAuth = () => {
-    return store.getState().auth.loggedIn
-}
+    const storeLoginState = store.getState().auth.loggedIn;
+    const [loggedIn, setLoggedIn] = useState(storeLoginState);
+
+    useEffect(() => {
+        setLoggedIn(storeLoginState);
+    }, [storeLoginState]);
+
+    return loggedIn;
+};
