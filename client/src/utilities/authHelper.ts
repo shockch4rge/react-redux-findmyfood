e512ec4a6bf29eecc6cdb8ae8da30a0354d758be
Shockch4rge <favouriteteo@gmail.com>
2022-01-22 20:52:04 +0800

    AuthHelper cleaned up

--- client/src/utilities/authHelper.ts
+++ client/src/utilities/authHelper.ts
@@ -1,4 +1,8 @@
 export class AuthHelper {
+    public static EMAIL_REGEX = new RegExp(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+
     public static isEmail(email: string) {
         const regex = new RegExp(
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
@@ -9,60 +13,38 @@ export class AuthHelper {
 
     public static isPassword(password: string) {
         const cases: boolean[] = [
-            hasDigits(password),
-            hasUppercaseLetters(password),
-            hasLowerCaseLetters(password),
-            hasLength(password),
-            hasSpecialCaseLetter(password),
+            this.hasDigits(password),
+            this.hasUppercaseLetters(password),
+            this.hasLowerCaseLetters(password),
+            this.hasLength(password),
+            this.hasSpecialCaseLetter(password),
         ];
 
         return cases.every(_case => _case);
     }
-}
 
-export function isEmail(email: string) {
-    const regex = new RegExp(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
+    public static hasUppercaseLetters(password: string, quantity: number = 2) {
+        const regex = new RegExp(`((.*[A-Z]){${quantity}})`);
+        return regex.test(password);
+    }
 
-    return regex.test(email);
-}
+    public static hasSpecialCaseLetter(password: string, quantity: number = 1) {
+        const regex = new RegExp(`((.*[!@#$&*]){${quantity}})`);
+        return regex.test(password);
+    }
 
-export const EMAIL_REGEX = new RegExp(
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-);
+    public static hasDigits(password: string, quantity: number = 2) {
+        const regex = new RegExp(`((.*[0-9])${quantity})`);
+        return regex.test(password);
+    }
 
-/**
- * Ensure string has:
- * 1. two uppercase letters
- * 2. one special case letter
- * 3. two digits
- * 4. three lowercase letters
- * 5. length of eight
- * @param password The input
- * @returns A check success/failure
- */
-export function hasUppercaseLetters(password: string, quantity: number = 2) {
-    const regex = new RegExp(`((.*[A-Z]){${quantity}})`);
-    return regex.test(password);
-}
+    public static hasLowerCaseLetters(password: string, quantity: number = 3) {
+        const regex = new RegExp(`((.*[a-z])${quantity})`);
+        return regex.test(password);
+    }
 
-export function hasSpecialCaseLetter(password: string, quantity: number = 1) {
-    const regex = new RegExp(`((.*[!@#$&*]){${quantity}})`);
-    return regex.test(password);
-}
-
-export function hasDigits(password: string, quantity: number = 2) {
-    const regex = new RegExp(`((.*[0-9])${quantity})`);
-    return regex.test(password);
-}
-
-export function hasLowerCaseLetters(password: string, quantity: number = 3) {
-    const regex = new RegExp(`((.*[a-z])${quantity})`);
-    return regex.test(password);
-}
-
-export function hasLength(password: string, length: number = 8) {
-    const regex = new RegExp(`.{${length}}`);
-    return regex.test(password);
+    public static hasLength(password: string, length: number = 8) {
+        const regex = new RegExp(`.{${length}}`);
+        return regex.test(password);
+    }
 }
