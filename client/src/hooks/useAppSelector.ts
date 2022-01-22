ceaac6a7172f543f6e25a41a536b8e7e6b03b74d
Shockch4rge <favouriteteo@gmail.com>
2022-01-22 20:55:39 +0800

    Updated comments

--- client/src/hooks/useAppSelector.ts
+++ client/src/hooks/useAppSelector.ts
@@ -1,9 +1,7 @@
 import { TypedUseSelectorHook, useSelector } from "react-redux";
-import { AppState } from "../app";
+import { AppState } from "../app/store";
 
 /**
  * An enhanced selector configured for the app's state.
- * @param state The state.
- * @returns A non-unknown type inferred from the return type of the callback.
  */
 export const useAppSelector: TypedUseSelectorHook<AppState> = useSelector;
