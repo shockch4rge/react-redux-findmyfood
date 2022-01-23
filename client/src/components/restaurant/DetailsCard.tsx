d6ce6e97ca4da2cccc66ca9b85608724a5fff197
Shockch4rge <favouriteteo@gmail.com>
2022-01-23 20:07:01 +0800

    Supporting detail cards for restaurant page

---
+++ client/src/components/restaurant/DetailsCard.tsx
@@ -0,0 +1,37 @@
+import { Box, Typography, Card, CardContent, Rating } from "@mui/material";
+import { RestaurantData } from "../../models/Restaurant";
+
+interface Props {
+    restaurant: RestaurantData;
+}
+
+const DetailsCard = ({ restaurant }: Props) => {
+    return (
+        <Box>
+            <Typography mb={1} variant="h5">
+                DETAILS
+            </Typography>
+            <Card
+                sx={{
+                    minWidth: 300,
+                    width: "fit-content",
+                    height: 300,
+                    borderRadius: 6,
+                    boxShadow: 3,
+                    p: 1,
+                }}
+            >
+                <CardContent>
+                    <Typography>Cost:</Typography>
+                    <Typography></Typography>
+                    <Typography>Cuisines:</Typography>
+                    <Typography fontFamily="GalyonBook">
+                        {restaurant.cuisines.join(", ")}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Box>
+    );
+};
+
+export default DetailsCard;
---
+++ client/src/components/restaurant/LocationCard.tsx
@@ -0,0 +1,56 @@
+import { Box, Typography, Card, CardContent, Stack } from "@mui/material";
+import config from "../../config.json";
+import { RestaurantData } from "../../models/Restaurant";
+
+interface Props {
+    restaurant: RestaurantData;
+}
+
+const LocationCard = ({ restaurant }: Props) => {
+    return (
+        <Box>
+            <Typography mb={1} variant="h5">
+                LOCATION AND CONTACT
+            </Typography>
+            <Card
+                sx={{
+                    minWidth: 300,
+                    width: "fit-content",
+                    height: 300,
+                    borderRadius: 6,
+                    boxShadow: 3,
+                    p: 1,
+                }}
+            >
+                <CardContent>
+                    <iframe
+                        height={120}
+                        frameBorder="0"
+                        src={`https://www.google.com/maps/embed/v1/place?key=${config.google.apis.maps}&q=New+York`}
+                        allowFullScreen
+                        style={{ borderRadius: 8 }}
+                    />
+                    <Stack spacing={1} mt={1}>
+                        <Box>
+                            <Typography>Location:</Typography>
+                            <Typography variant="body2">{restaurant.name}</Typography>
+                        </Box>
+                        <Box>
+                            <Typography>Opening Hours:</Typography>
+                            <Typography variant="body2">
+                                {restaurant.availableTimes.openingHours} -{" "}
+                                {restaurant.availableTimes.closingHours}
+                            </Typography>
+                        </Box>
+                        <Box>
+                            <Typography>Telephone:</Typography>
+                            <Typography variant="body2">{restaurant.telephone}</Typography>
+                        </Box>
+                    </Stack>
+                </CardContent>
+            </Card>
+        </Box>
+    );
+};
+
+export default LocationCard;
