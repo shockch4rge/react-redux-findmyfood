a791bb8671b123292e85d8316649840e86348ac2
Shockch4rge <favouriteteo@gmail.com>
2022-01-26 13:57:20 +0800

    The return of ReviewCard and ReplyCard

---
+++ client/src/components/restaurant/ReplyCard.tsx
@@ -0,0 +1,57 @@
+import { Box, Card, Container, Avatar, Typography, Divider } from "@mui/material";
+import { useGetUserQuery } from "../../app/services/users";
+import { useGetUserAvatarQuery } from "../../app/services/images";
+import { ReplyData } from "../../models/Reply";
+import User from "../../models/User";
+
+interface Props {
+    reply: ReplyData;
+}
+
+const ReplyCard = ({ reply }: Props) => {
+    // const { isLoading: userLoading, data: user } = useGetUserQuery(reply.userId);
+    // const { isLoading: avatarLoading, data: userAvatar } = useGetUserAvatarQuery(reply.userId);
+
+    // #region marked for deletion
+    const mockUser = User.getMockUser();
+    // #endregion
+
+    return (
+        <Box display="flex" justifyContent="flex-end">
+            <Card
+                sx={{
+                    width: "80%",
+                    display: "flex",
+                    height: "fit-content",
+                    borderRadius: 3,
+                    p: 1,
+                }}
+            >
+                <Container sx={{ width: "fit-content" }}>
+                    <Box display="flex" justifyContent="center" alignItems="center">
+                        <Avatar sx={{ width: 50, height: 50, mb: 1 }} src={mockUser?.avatarPath} />
+                    </Box>
+                    <Typography textAlign="center">{mockUser?.username}</Typography>
+                </Container>
+                <Divider orientation="vertical" flexItem sx={{ ml: 1, mr: 3 }} />
+                <Box width="100%">
+                    <Typography variant="body2" fontSize={16}>
+                        {reply.content}
+                    </Typography>
+                    <Typography
+                        m={1}
+                        textAlign="end"
+                        sx={{
+                            fontFamily: "GalyonBook",
+                            fontStyle: "italic",
+                        }}
+                    >
+                        {reply.isEdited ? "Edited" : "Posted"} {reply.timestamp.toDateString()}
+                    </Typography>
+                </Box>
+            </Card>
+        </Box>
+    );
+};
+
+export default ReplyCard;
---
+++ client/src/components/restaurant/ReviewCard.tsx
@@ -0,0 +1,67 @@
+import AddIcon from "@mui/icons-material/Add";
+import { Card, Container, Box, Avatar, Typography, Divider, Rating, Button } from "@mui/material";
+import { useGetUserAvatarQuery } from "../../app/services/images";
+import { useGetReviewRepliesQuery } from "../../app/services/replies";
+import Reply, { ReplyData } from "../../models/Reply";
+import { ReviewData } from "../../models/Review";
+import User from "../../models/User";
+import ReplyCard from "./ReplyCard";
+
+interface Props {
+    review: ReviewData;
+}
+
+const ReviewCard = ({ review }: Props) => {
+    // const { isLoading: userLoading, data: user } = useGetUserQuery(review.userId);
+    // const { isLoading: repliesLoading, data: replies } = useGetReviewRepliesQuery(review.id);
+    // const { isLoading: avatarLoading, data: userAvatar } = useGetUserAvatarQuery(review.userId);
+
+    // #region marked for deletion
+    const mockUser = User.getMockUser();
+    const replies = new Array(10).fill(Reply.getMockReply()) as ReplyData[];
+    // #endregion
+
+    return (
+        <>
+            <Card
+                sx={{
+                    width: "100%",
+                    display: "flex",
+                    height: "fit-content",
+                    borderRadius: 3,
+                    p: 2,
+                }}
+            >
+                <Container sx={{ width: "fit-content" }}>
+                    <Box display="flex" justifyContent="center" alignItems="center">
+                        <Avatar sx={{ width: 80, height: 80, mb: 1 }} />
+                    </Box>
+                    <Typography textAlign="center">{mockUser?.username}</Typography>
+                </Container>
+                <Divider orientation="vertical" flexItem sx={{ ml: 2, mr: 4 }} />
+                <Box width="100%">
+                    <Typography variant="h5">{review.title}</Typography>
+                    <Rating readOnly value={review.rating} precision={0.5} sx={{ my: 1 }} />
+                    <Typography variant="body2" fontSize={16}>
+                        {review.content}
+                    </Typography>
+                    <Typography
+                        m={1}
+                        textAlign="end"
+                        sx={{ fontFamily: "GalyonBook", fontStyle: "italic" }}
+                    >
+                        {review.isEdited ? "Edited" : "Posted"} {review.timestamp.toDateString()}
+                    </Typography>
+                    <Button onClick={() => {}} startIcon={<AddIcon />}>
+                        Add Reply
+                    </Button>
+                </Box>
+            </Card>
+            {replies.slice(0, 2).map(reply => (
+                <ReplyCard reply={reply} />
+            ))}
+        </>
+    );
+};
+
+export default ReviewCard;
