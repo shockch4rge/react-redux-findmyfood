dc9fa66e0cfa2c39e7d4b4063b5e43b1410cc583
Shockch4rge <favouriteteo@gmail.com>
2022-01-29 14:10:55 +0800

    Attempt at debouncing search queries

---
+++ client/src/hooks/useDebounce.ts
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+export const useDebounce = <T = string>(value: T, delay: number = 300) => {
+    const [debouncedValue, setDebouncedValue] = useState<T>(value);
+    
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            setDebouncedValue(value);
+        }, delay);
+
+        return () => clearTimeout(handler);
+    }, [value, delay]);
+
+    return debouncedValue;
+};
