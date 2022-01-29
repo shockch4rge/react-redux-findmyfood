41a5f48065e3e96d2011b53b29c68410cebd9978
Shockch4rge <favouriteteo@gmail.com>
2022-01-29 14:13:13 +0800

    Removed pointless useAvatar hook

--- client/src/hooks/useAvatar.ts
+++
@@ -1,7 +0,0 @@
-import { useGetUserAvatarQuery } from "../app/services/images";
-import config from "../config.json";
-
-export const useAvatar = (userId: string) => {
-    const { data: avatarPath } = useGetUserAvatarQuery(userId);
-    return `${config.api.baseUrl}/uploads/${avatarPath}`;
-};
