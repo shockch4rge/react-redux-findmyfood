e37971194766556f8f68f2d941238e123acb47e5
Shockch4rge <favouriteteo@gmail.com>
2022-01-29 19:19:58 +0800

    Added optional anchorOrigin prop

--- client/src/components/common/Snack.tsx
+++ client/src/components/common/Snack.tsx
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
-import { Snackbar, Alert, AlertProps } from "@mui/material";
+import { Snackbar, Alert, AlertProps, SnackbarProps } from "@mui/material";
 
 interface Props {
-    severity?: AlertProps["severity"];
+    severity: AlertProps["severity"];
     message: string;
     open: boolean;
     onClose: () => void;
+    anchorOrigin?: SnackbarProps["anchorOrigin"];
 }
 
-const Snack = ({ message, severity, open, onClose }: Props) => {
+const Snack = ({ message, severity, open, onClose, anchorOrigin }: Props) => {
     return (
-        <Snackbar open={open} autoHideDuration={6000} onClose={onClose}>
+        <Snackbar open={open} anchorOrigin={anchorOrigin} autoHideDuration={6000} onClose={onClose}>
             <Alert onClose={onClose} severity={severity} sx={{ width: "100%" }}>
                 {message}
             </Alert>
