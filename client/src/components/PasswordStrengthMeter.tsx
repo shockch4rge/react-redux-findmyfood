1f35f1c9977c6c55f9242dd0ff9e44056636c062
Shockch4rge <favouriteteo@gmail.com>
2022-02-01 05:31:54 +0800

    Component to gauge password strength

---
+++ client/src/components/PasswordStrengthMeter.tsx
@@ -0,0 +1,46 @@
+import { Stack, Box, Typography } from "@mui/material";
+import { useState, useEffect } from "react";
+
+interface Props {
+    sections: {
+        color: string;
+        valid: boolean;
+        criteria: string;
+    }[];
+    finalValidation: boolean;
+}
+
+const PasswordStrengthMeter = ({ sections, finalValidation }: Props) => {
+    const [strength, setStrength] = useState(0);
+
+    useEffect(() => {
+        const possibleStrength = sections.filter(section => section.valid).length;
+
+        if (possibleStrength !== sections.length) {
+            setStrength(possibleStrength);
+        }
+    }, [sections]);
+
+    return (
+        <>
+            <Stack direction="row" spacing={0.5} justifyContent="space-between" mb={1}>
+                {sections.map(({ color, valid }) => (
+                    <Box
+                        key={`${color}-${color}`}
+                        height={4}
+                        width={`${100 / sections.length}%`}
+                        sx={{
+                            backgroundColor: theme =>
+                                finalValidation ? theme.palette.primary.main : valid ? color : "#E0E0E0",
+                        }}
+                    />
+                ))}
+            </Stack>
+            <Typography variant="body2" color={finalValidation ? "success" : "error"}>
+                {finalValidation ? "Password is strong!" : `Password Criteria: ${sections[strength].criteria}`}
+            </Typography>
+        </>
+    );
+};
+
+export default PasswordStrengthMeter;
