982c5578ef869a0d273057c655b16fe66baeea25
Shockch4rge <favouriteteo@gmail.com>
2022-02-01 05:36:54 +0800

    Cleaned up AuthHelper

--- client/src/utilities/authHelper.ts
+++ client/src/utilities/authHelper.ts
@@ -1,8 +1,4 @@
 export class AuthHelper {
-    public static EMAIL_REGEX = new RegExp(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
-
     public static isEmail(email: string) {
         const regex = new RegExp(
             /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
@@ -14,21 +10,20 @@ export class AuthHelper {
     public static isPassword(password: string) {
         const cases: boolean[] = [
             this.hasDigits(password),
-            this.hasUppercaseLetters(password),
-            this.hasLowerCaseLetters(password),
+            this.hasUpperCaseLetters(password),
             this.hasLength(password),
-            this.hasSpecialCaseLetter(password),
+            this.hasSpecialCharacters(password),
         ];
 
         return cases.every(_case => _case);
     }
 
-    public static hasUppercaseLetters(password: string, quantity: number = 2) {
+    public static hasUpperCaseLetters(password: string, quantity: number = 2) {
         const regex = new RegExp(`((.*[A-Z]){${quantity}})`);
         return regex.test(password);
     }
 
-    public static hasSpecialCaseLetter(password: string, quantity: number = 1) {
+    public static hasSpecialCharacters(password: string, quantity: number = 1) {
         const regex = new RegExp(`((.*[!@#$&*]){${quantity}})`);
         return regex.test(password);
     }
