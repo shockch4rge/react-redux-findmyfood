1b0dd049afbe9502418c6e60b5bdb9aa75a93c8b
Shockch4rge <favouriteteo@gmail.com>
2022-02-02 04:12:04 +0800

    RestaurantPage SHOULD be bugs free

--- client/src/pages/RestaurantPage.tsx
+++ client/src/pages/RestaurantPage.tsx
@@ -12,6 +12,11 @@ import {
     Button,
     Skeleton,
 } from "@mui/material";
+import {
+    setShowDeleteReviewDialog,
+    setShowEditReviewDialog,
+    setShowWriteReviewDialog,
+} from "../app/slices/ui/dialogs/reviewDialog";
 import { useAppDispatch } from "../hooks/useAppDispatch";
 import { useParams, useNavigate } from "react-router-dom";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
@@ -21,31 +26,21 @@ import React, { useEffect, useState } from "react";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import AddIcon from "@mui/icons-material/Add";
-import {
-    setShowDeleteReviewDialog,
-    setShowEditReviewDialog,
-    setShowWriteReviewDialog,
-} from "../app/slices/ui/dialogs/reviewDialog";
 import EditReviewDialog from "../components/dialogs/review/EditReviewDialog";
 import WriteReviewDialog from "../components/dialogs/review/WriteReviewDialog";
 import DetailsCard from "../components/restaurant/DetailsCard";
 import LocationCard from "../components/restaurant/LocationCard";
 import RatingsCard from "../components/restaurant/RatingsCard";
 import ReviewCard from "../components/restaurant/ReviewCard";
-import {
-    useGetRestaurantQuery,
-    useUpdateRestaurantRatingMutation,
-} from "../app/services/restaurants";
+import { useGetRestaurantQuery, useUpdateRestaurantRatingMutation } from "../app/services/restaurants";
 import {
     useGetRestaurantReviewsQuery,
     useLazyGetReviewByUserAndRestaurantIdQuery,
 } from "../app/services/reviews";
 import DeleteReviewDialog from "../components/dialogs/review/DeleteReviewDialog";
-import { ReviewData } from "../models/Review";
 import Footer from "../components/Footer";
 import RestaurantPageSkeleton from "../components/skeletons/RestaurantPageSkeleton";
 import { setShowLoginDialog } from "../app/slices/ui/dialogs/loginDialog";
-import { createSnack } from "../app/slices/ui/snackbars/snack";
 
 const RestaurantPage = () => {
     const navigate = useNavigate();
@@ -53,48 +48,35 @@ const RestaurantPage = () => {
     const restaurantId = useParams().restaurantId!;
     const user = useAppSelector(state => state.auth);
 
-    const [userReview, setUserReview] = useState<ReviewData | undefined>(undefined);
+    const { isLoading: restaurantLoading, data: restaurant } = useGetRestaurantQuery(restaurantId);
+    const { isLoading: reviewsLoading, data: reviews } = useGetRestaurantReviewsQuery(restaurantId);
 
-    const {
-        isLoading: restaurantLoading,
-        data: restaurant,
-        error,
-    } = useGetRestaurantQuery(restaurantId);
-
-    const {
-        isLoading: reviewsLoading,
-        data: reviews,
-        refetch: refetchReviews,
-    } = useGetRestaurantReviewsQuery(restaurantId);
-
-    const [fetchUserReview] = useLazyGetReviewByUserAndRestaurantIdQuery();
     const [updateRestaurantRating] = useUpdateRestaurantRatingMutation();
+    const [fetchUserReview, { data: userReview }] = useLazyGetReviewByUserAndRestaurantIdQuery();
 
+    // user logged in
     useEffect(() => {
         if (user) {
-            fetchUserReview({ userId: user.id, restaurantId: restaurantId })
-                .unwrap()
-                .then(setUserReview)
-                .catch(console.log);
+            fetchUserReview({ userId: user.id, restaurantId });
         }
     }, [user]);
 
-    const onPost = () => {
-        refetchReviews();
-        fetchUserReview({ userId: user!.id, restaurantId: restaurantId });
-        updateRestaurantRating({
-            id: restaurantId,
-            rating: +(reviews!.reduce((acc, review) => +acc + +review.rating, 0) / reviews!.length),
-        });
-    };
+    // number of reviews changed
+    useEffect(() => {
+        if (reviews) {
+            fetchUserReview({ userId: user?.id, restaurantId });
+            updateRestaurantRating({
+                id: restaurantId,
+                rating: reviews!.reduce((acc, review) => +acc + +review.rating, 0) / reviews.length,
+            });
+        }
+    }, [reviews]);
 
     const isLoading = restaurantLoading || reviewsLoading || !restaurant || !reviews;
 
     return (
         <>
-            {error ? (
-                <>Oh no, something went wrong!</>
-            ) : isLoading ? (
+            {isLoading ? (
                 <RestaurantPageSkeleton />
             ) : (
                 <>
@@ -116,12 +98,8 @@ const RestaurantPage = () => {
                                 background: `linear-gradient(to top, #161616, transparent), url(${restaurant.imageUrl}) center center no-repeat`,
                                 backgroundSize: "cover",
                                 overflow: "hidden",
-                            }}
-                        >
-                            <Typography
-                                variant="h3"
-                                sx={{ color: theme => theme.palette.common.white }}
-                            >
+                            }}>
+                            <Typography variant="h3" sx={{ color: theme => theme.palette.common.white }}>
                                 {restaurant.name}
                             </Typography>
                         </Card>
@@ -130,12 +108,11 @@ const RestaurantPage = () => {
 
                         <Stack
                             direction={{ xs: "column", md: "row" }}
-                            spacing={{ xs: 1 }}
                             justifyContent="space-between"
-                            mb={7}
-                        >
-                            <RatingsCard restaurant={restaurant} reviews={reviews} />
-                            <WriteReviewDialog restaurantId={restaurant.id} onPost={onPost} />
+                            spacing={{ xs: 1 }}
+                            mb={7}>
+                            <RatingsCard restaurant={restaurant} reviews={reviews} userReview={userReview} />
+                            <WriteReviewDialog restaurantId={restaurant.id} />
                             <DetailsCard restaurant={restaurant} />
                             <LocationCard restaurant={restaurant} />
                         </Stack>
@@ -165,18 +142,13 @@ const RestaurantPage = () => {
                                         direction="row"
                                         display="flex"
                                         justifyContent="center"
-                                        alignItems="center"
-                                    >
+                                        alignItems="center">
                                         <Button
                                             variant="outlined"
-                                            onClick={() => dispatch(setShowLoginDialog(true))}
-                                        >
+                                            onClick={() => dispatch(setShowLoginDialog(true))}>
                                             Login
                                         </Button>
-                                        <Button
-                                            variant="outlined"
-                                            onClick={() => navigate("/register")}
-                                        >
+                                        <Button variant="outlined" onClick={() => navigate("/register")}>
                                             Sign Up
                                         </Button>
                                     </Stack>
@@ -195,15 +167,11 @@ const RestaurantPage = () => {
                                     <Button
                                         variant="outlined"
                                         onClick={() => dispatch(setShowWriteReviewDialog(true))}
-                                        sx={{ m: 3 }}
-                                    >
+                                        sx={{ m: 3 }}>
                                         Write a review
                                     </Button>
 
-                                    <WriteReviewDialog
-                                        restaurantId={restaurant.id}
-                                        onPost={onPost}
-                                    />
+                                    <WriteReviewDialog restaurantId={restaurant.id} />
                                 </Stack>
                             )}
 
@@ -216,14 +184,9 @@ const RestaurantPage = () => {
                                         height: "fit-content",
                                         borderRadius: 3,
                                         p: 2,
-                                    }}
-                                >
+                                    }}>
                                     <Container sx={{ width: "fit-content" }}>
-                                        <Box
-                                            display="flex"
-                                            justifyContent="center"
-                                            alignItems="center"
-                                        >
+                                        <Box display="flex" justifyContent="center" alignItems="center">
                                             <Avatar
                                                 sx={{ width: 80, height: 80, mb: 1 }}
                                                 src={user.avatarPath}
@@ -231,16 +194,12 @@ const RestaurantPage = () => {
                                         </Box>
                                         <Typography textAlign="center">{user.username}</Typography>
                                     </Container>
-                                    <Divider
-                                        orientation="vertical"
-                                        flexItem
-                                        sx={{ ml: 2, mr: 4 }}
-                                    />
+                                    <Divider orientation="vertical" flexItem sx={{ ml: 2, mr: 4 }} />
                                     <Box width="100%">
                                         <Typography variant="h5">{userReview.title}</Typography>
                                         <Rating
                                             readOnly
-                                            value={userReview.rating}
+                                            value={+userReview.rating}
                                             precision={0.5}
                                             sx={{ my: 1 }}
                                         />
@@ -250,49 +209,27 @@ const RestaurantPage = () => {
                                         <Typography
                                             m={1}
                                             textAlign="end"
-                                            sx={{ fontFamily: "GalyonBook", fontStyle: "italic" }}
-                                        >
+                                            sx={{ fontFamily: "GalyonBook", fontStyle: "italic" }}>
                                             {userReview.timestamp}
                                         </Typography>
 
                                         <Stack direction="row" spacing={3}>
                                             <Button
-                                                onClick={() =>
-                                                    dispatch(setShowEditReviewDialog(true))
-                                                }
-                                                startIcon={<EditIcon />}
-                                            >
+                                                onClick={() => dispatch(setShowEditReviewDialog(true))}
+                                                startIcon={<EditIcon />}>
                                                 Edit Review
                                             </Button>
 
-                                            <EditReviewDialog review={userReview} onPost={onPost} />
+                                            <EditReviewDialog review={userReview} />
 
                                             <Button
-                                                onClick={() =>
-                                                    dispatch(setShowDeleteReviewDialog(true))
-                                                }
-                                                startIcon={<DeleteIcon />}
-                                            >
+                                                onClick={() => dispatch(setShowDeleteReviewDialog(true))}
+                                                startIcon={<DeleteIcon />}>
                                                 Delete Review
                                             </Button>
-                                        </Stack>
 
-                                        <DeleteReviewDialog
-                                            reviewId={userReview.id}
-                                            onDelete={() => {
-                                                refetchReviews();
-                                                setUserReview(undefined);
-                                                updateRestaurantRating({
-                                                    id: restaurantId!,
-                                                    rating: +(
-                                                        reviews!.reduce(
-                                                            (acc, review) => +acc + +review.rating,
-                                                            0
-                                                        ) / reviews!.length
-                                                    ),
-                                                });
-                                            }}
-                                        />
+                                            <DeleteReviewDialog reviewId={userReview.id} />
+                                        </Stack>
                                     </Box>
                                 </Card>
                             )}
@@ -305,20 +242,13 @@ const RestaurantPage = () => {
 
                             <Stack spacing={3}>
                                 {reviews &&
-                                    reviews.map(review => (
-                                        <ReviewCard key={review.id} review={review} />
-                                    ))}
+                                    reviews.map(review => <ReviewCard key={review.id} review={review} />)}
 
                                 {reviews.length <= 0 && (
-                                    <Box
-                                        mb={10}
-                                        display="flex"
-                                        justifyContent="center"
-                                        alignItems="center"
-                                    >
+                                    <Box mb={10} display="flex" justifyContent="center" alignItems="center">
                                         <Typography fontSize={16} variant="body2">
-                                            There are no reviews for this restaurant yet... you can
-                                            be the first!
+                                            There are no reviews for this restaurant yet... you can be the
+                                            first!
                                         </Typography>
                                     </Box>
                                 )}
