b7d02578f32d549dc70df6443bc2b77a7b9db71d
Shockch4rge <favouriteteo@gmail.com>
2022-02-05 01:09:25 +0800

    Very basic footer

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from "@mui/material";
+
+const Footer = () => {
+    return (
+        <Box mt={30} width="100%" height={150} sx={{ background: "#2F2F2F" }}>
+            <Typography>Hello World!</Typography>
+
+        </Box>
+    );
+};
+
+export default Footer;
