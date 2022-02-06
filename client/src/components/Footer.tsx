722f6008db199ec83702707b9025422aedbb503f
Shockch4rge <favouriteteo@gmail.com>
2022-02-06 19:13:54 +0800

    Footer contains GitHub redirect to this repo

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,9 +1,21 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Stack, IconButton } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 const Footer = () => {
     return (
-        <Box mt={30} width="100%" height={150} sx={{ background: "#2F2F2F" }}>
-            <Typography>Hello World!</Typography>
+        <Box
+            mt={30}
+            width="100%"
+            height={150}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            sx={{ background: "#2F2F2F" }}>
+            <Stack direction="row" spacing={3}>
+                <IconButton onClick={() => window.open("https://github.com/Shockch4rge/react-redux-findmyfood")}>
+                    <GitHubIcon fontSize="large"/>
+                </IconButton>
+            </Stack>
 
         </Box>
     );
